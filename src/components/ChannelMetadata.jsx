21d06e808b14545276811e15e580235adb376cec
rvd2pwipwip <herve_desrosiers@hotmail.com>
2025-04-11 07:39:07 -0400

    channel info layout

--- src/components/ChannelMetadata.jsx
+++ src/components/ChannelMetadata.jsx
@@ -5,7 +5,8 @@ const ChannelMetadata = ({ title, description, tags }) => {
   return (
     <div className="channel-metadata">
       <div className="thumbnail-placeholder"></div>
-      <h1 className="channel-title">{title}</h1>
+      <div className="channel-content">
+        <h1 className="channel-title">{title}</h1>
         <div className="channel-actions">
           <button className="play-button">Play</button>
           <button className="favorites-button">Add to Favorites</button>
@@ -16,6 +17,7 @@ const ChannelMetadata = ({ title, description, tags }) => {
             <span key={index} className="channel-tag">{tag}</span>
           ))}
         </div>
+      </div>
     </div>
   );
 };
