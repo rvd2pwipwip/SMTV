f4819fe4e9c781577dae1629c4cbe6e615de3836
rvd2pwipwip <herve_desrosiers@hotmail.com>
2025-04-14 09:08:11 -0400

    channel info styles

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import { useScreen } from '../contexts/ScreenContext';
 import { useNavigation } from '../contexts/NavigationContext';
-import ChannelMetadata from './ChannelMetadata';
 import ScreenLayout from './ScreenLayout';
 import './ChannelInfo.css';
-import './ChannelMetadata.css';
 
 const ChannelInfo = () => {
   const { selectedChannel, goBack } = useScreen();
@@ -28,6 +26,16 @@ const ChannelInfo = () => {
     handleKeyNavigation(e);
   };
 
+  const handlePlay = () => {
+    // TODO: Implement play functionality
+    console.log('Play clicked');
+  };
+
+  const handleAddToFavorites = () => {
+    // TODO: Implement add to favorites functionality
+    console.log('Add to favorites clicked');
+  };
+
   return (
     <ScreenLayout title={`${selectedChannel?.title || 'Channel'} Info`}>
       <div 
@@ -37,11 +45,49 @@ const ChannelInfo = () => {
         onKeyDown={handleKeyDown}
         autoFocus
       >
-        <ChannelMetadata
-          title={selectedChannel?.title || 'Channel Title'}
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          tags={['Cooking', 'Love', 'Third', 'Fourth Tag', 'Fifth']}
-        />
+        <div className="channel-info__content">
+          <div className="channel-info__thumbnail">
+            {/* Placeholder for channel thumbnail/preview */}
+          </div>
+          
+          <div className="channel-info__details">
+            <h1 className="channel-info__title">
+              {selectedChannel?.title || 'The Very Best Channel Ever Programmed'}
+            </h1>
+            
+            <div className="channel-info__actions">
+              <button 
+                className="tv-button tv-button--primary"
+                onClick={handlePlay}
+              >
+                <span className="icon">▶</span>
+                Play
+              </button>
+              
+              <button 
+                className="tv-button tv-button--outline"
+                onClick={handleAddToFavorites}
+              >
+                <span className="icon">♡</span>
+                Add to Favorites
+              </button>
+            </div>
+            
+            <p className="channel-info__description">
+              Lorem ipsum dolor sit amet consectetur. A sed in neque urna etiam eu a. 
+              Imperdiet et felis porttitor risus. Tortor aliquet facilisis id integer eget. 
+              Risus nunc turpis euismod et ultrices varius. Ultrices enim nisi praesent tellus ut consequat magna eget ac.
+            </p>
+            
+            <div className="channel-info__tags">
+              {['Cooking', 'Love', 'Third', 'Fourth Tag', 'Fifth'].map((tag, index) => (
+                <span key={index} className="tv-tag" tabIndex="0">
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </ScreenLayout>
   );
